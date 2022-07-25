1c913aef20b9ce29afc3677909a088fb8069c2ad
Hassanabdelqader <terawihassan@gmail.com>
2022-07-25 21:45:37 +0300

    Update

--- src/header.js
+++ src/header.js
@@ -1,8 +1,27 @@
 import  React  from "react";
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 class Header extends React.Component  {
     render() {
-      return <h1>Horned Animal App</h1>;
+      return (
+        <Navbar bg="dark" variant="dark" expand="lg">
+        <Container>
+          <Navbar.Brand href="#home">Horned Beast</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link href="#home">Home</Nav.Link>
+              <Nav.Link href="#link">Aboute</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+  
+      )
     }
   }
   export default Header;
