847e8c1dba8016e709fb0293114d29cf8da5fcb5
Hassanabdelqader <terawihassan@gmail.com>
2022-07-25 23:26:58 +0300

    Add Events

--- src/HornedBeast.js
+++ src/HornedBeast.js
@@ -4,35 +4,70 @@ import Card from 'react-bootstrap/Card';
 import './HornedBeast.css';
 
 class HornedBeast extends React.Component {
-    constructor(props) {
 
+
+    constructor(props) {
         super(props);
         this.state = {
-            favorited: 0
+            favorited: 0,
+            titleColor: false,
+            disColor: false,
         }
     }
 
     getState = () => {
         return this.state.favorited;
     }
+
+
     setStatemy = () => {
         this.setState({
             favorited: this.state.favorited + 1
         });
     }
+    getColortitle = () => {
+        return this.state.titleColor;
+    }
+    setColor = () => {
+        this.setState({
+            titleColor: true
+        });
+
+    }
+    getColorDis = () => {
+        return this.state.disColor;
+    }
+    setColorDis = () => {
+        this.setState({
+            disColor: true
+        });
+
+    }
+
 
     render() {
-        return <div>
-            <Card  style={{ width: '18rem'}}>
+        return <div className="CardDiv2">
+            <Card style={{ width: '18rem' }}>
                 <Card.Img variant="top" src={this.props.imageUrl} onClick={this.setStatemy} width="300" height="400" />
                 <Card.Body>
-                    <Card.Title>{this.props.title}</Card.Title>
-                    <Card.Text>
-                        {this.props.description}
+                    <Card.Title
+                        style={{
+                            color: this.getColortitle() ? 'red' : 'black',
+                        }}
+                        onClick={() => {
+                            this.setColor();
+                        }}>{this.props.title}</Card.Title>
+                    <Card.Text
+                        style={{
+                            color: this.getColorDis() ? 'grey' : 'black',
+                        }}
+                        onClick={() => {
+                            this.setColorDis();
+                        }}>{this.props.description}
                     </Card.Text>
                     <Button variant="primary" onClick={this.setStatemy}>Love </Button>
                     {` ${this.getState()} `}  ❤ Favorited
-                    
+
                 </Card.Body>
             </Card>
         </div>
