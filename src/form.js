35233ad4c4f9639c7c83545813d91a48bf6cd558
Hassanabdelqader <terawihassan@gmail.com>
2022-07-27 18:08:14 +0300

    add 100

--- src/form.js
+++ src/form.js
@@ -20,6 +20,8 @@ export default class Forms extends React.Component{
             <option value="1" >One</option>
             <option value="2">Two</option>
             <option value="3">Three</option>
+            <option value="100">100</option>
+           
           </Form.Select>
         )
     }
